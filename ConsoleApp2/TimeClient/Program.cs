8a2851a2f1381d01532f19cff75d8071f8be2870
Rohan Sanjay Mahajan <rohansanjay.mahajan@kaplan.edu>
2023-08-17 12:10:15 +0530

    Add project files.

---
+++ ConsoleApp2/TimeClient/Program.cs
@@ -0,0 +1,33 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+//using GrpcService2;
+using Grpc.Core;
+using ConsoleApp2;
+
+
+
+namespace TimeClient
+{
+    internal class Program
+    {
+        static void  Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5276");
+
+           // var channel = new Channel($"{ServerAddress}:{ServerPort}", ChannelCredentials.Insecure);
+             var client = new Time.TimeClient(channel);
+
+            var response = client.GetCurrentTime(new Empty());
+
+            Console.WriteLine($"Current Time: {response.CurrentTime}");
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+
+    }
+}*/
---
+++ GrpcService2/TimeServer/TimeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcService2;
+
+namespace GrpcService2.Services
+{
+    public class TimeService: Time.TimeBase
+    {
+        public override Task<TimeResponse> GetCurrentTime(Empty request, ServerCallContext context)
+        {
+            // var currentTime = DateTime.Now.ToString("HH:mm:ss");
+            // return new TimeResponse { CurrentTime = currentTime };
+            return Task.FromResult(new TimeResponse {
+                CurrentTime = DateTime.Now.ToString("HH:mm:ss"),
+                CurrentDate = DateTime.Now.ToString("dd:MM:yyyy")
+            }); 
+        }
+    }
+}
